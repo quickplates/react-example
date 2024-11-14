513c5058f652738f5b787aabb50ae6a6de640171
Sebastian Pietras <github@mail.spietras.dev>
2024-11-14 18:39:33 +0100

    Added template files (#31)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const colorSchemeStorageKey = "react-example-color-scheme";
+export const defaultColorScheme = "auto";
+export const defaultLocale = "en";
+export const defaultUserLanguage = "en";
+export const stateStorageKey = "react-example-state";
