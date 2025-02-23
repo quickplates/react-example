ab3c09a6aae5f8d603a117cd991c948728201834
Sebastian Pietras <github@mail.spietras.dev>
2025-02-23 21:32:53 +0100

    Added template files (#32)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/components/views/root/root-error-view/main.tsx
@@ -0,0 +1,11 @@
+import { msg } from "@lingui/core/macro";
+import { useLingui } from "@lingui/react";
+import { Title } from "@mantine/core";
+
+import { RootErrorViewInput } from "./types";
+
+export function RootErrorView({}: RootErrorViewInput) {
+  const { _ } = useLingui();
+
+  return <Title>{_(msg({ message: "Something went wrong" }))}</Title>;
+}
---
+++ src/router/main.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+import { routes } from "../app/routes";
+import { options } from "./options";
+import { RouterInput } from "./types";
+
+const router = createBrowserRouter(routes, options);
+
+export function Router({}: RouterInput) {
+  return <RouterProvider router={router} />;
+}
